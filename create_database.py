1a60adcd0b18c7a368e02552be4b59c19351cdec
tristanh314 <tristanh314@gmail.com>
2020-05-20 19:05:10 -0700

    Refactored database creation

--- create_database.py
+++ create_database.py
@@ -1,41 +1,48 @@
 # Import dependencies.
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData, Table, Column, Integer, String, Float
+from sqlalchemy import Table, Column, Integer, String, Float
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+# Create the Listings class.
+class Listing(Base):
+    __tablename__ = "listings"
+    id = Column(Integer, primary_key=True)
+    address = Column(String(255))
+    price = Column(Integer)
+    home_type = Column(String(255))
+    bedrooms = Column(Integer)
+    bathrooms = Column(Float)
+    square_feet = Column(Integer)
+    built = Column(Integer)
+    neighborhood = Column(String(255))
+    county = Column(String(255))
+    city = Column(String(255))
+    zipcode = Column(Integer)
+    high_school = Column(String(255))
+    middle_school = Column(String(255))
+    elementary_school = Column(String(255))
 
 # Create the database connection.
 database_path = "Resources/housing.sqlite"
 engine = create_engine(f"sqlite:///{database_path}")
-meta = MetaData(engine)
 conn = engine.connect()
+session = Session(bind=engine)
 
-# Create the listings table.
-listings = Table(
-    "listings", meta,
-    Column("id", Integer, primary_key=True),
-    Column("address", String(255)),
-    Column("price", Integer),
-    Column("home_type", String(255)),
-    Column("bedrooms", Integer),
-    Column("bathrooms", Float),
-    Column("square_feet", Integer),
-    Column("built", Integer),
-    Column("neighborhood", String(255)),
-    Column("county", String(255)),
-    Column("city", String(255)),
-    Column("zipcode", Integer),
-    Column("high_school", String(255)),
-    Column("middle_school", String(255)),
-    Column("elementary_school", String(255))
-)
-meta.create_all()
+# Clear out current data in the database.
+Base.metadata.drop_all(engine)
+
+# Create a metadata layer that abstracts the database.
+Base.metadata.create_all(engine)
 
 # Store the scraped data as a data frame.
 scraped_data = pd.read_csv("Resources/housingData.csv")
 
 # Insert data into the database.
 for _, row in scraped_data.iterrows():
-    ins = listings.insert().values(
+    listing = Listing(
       address = row["address"],
       price = row["price"],
       home_type = row["home_type"],
@@ -51,7 +58,13 @@ for _, row in scraped_data.iterrows():
       middle_school = row["middle_school"],
       elementary_school = row["elementary_school"]
       )
-    conn.execute(ins)
+    session.add(listing)
+
+# Commit all listings
+session.commit()
+
+# Close the session.
+session.close()
 
 # Close the connection.
 conn.close()
