0cf2cbcec8838168b9d360449c149c32f3c15919
tristanh314 <tristanh314@gmail.com>
2020-05-30 13:25:23 -0700

    Preparing files for Heroku deployment.

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import requests
-from flask_pymongo import PyMongo
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
